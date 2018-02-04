0bff2ae940be45bc042a51350d847522ab7c52ae
MichalBroucek <michalbroucek@gmail.com>
2018-02-04 21:51:00 +0100

    Change import to be able to run it from cmd

--- canSend.py
+++ canSend.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
-from src import param
-from src import can_simulator
+import src.param
+import src.can_simulator
 
-#can_interface = 'vcan0'
-can_interface = 'can0'
+can_interface = 'vcan0'
+#can_interface = 'can0'
 
 if __name__ == "__main__":
-    param = param.Param()
+    param = src.param.Param()
     simulator_parameters = param.parse_cmd_params(sys.argv)
 
     if simulator_parameters is None:
         exit()
 
-    simulator = can_simulator.CanSimulator(simulator_parameters, can_interface)
+    simulator = src.can_simulator.CanSimulator(simulator_parameters, can_interface)
     simulator.run_action()
 
     print("- Done -")
